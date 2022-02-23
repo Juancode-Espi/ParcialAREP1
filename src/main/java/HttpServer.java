68d72748a1eae2a1d9bbb9a10403b05ade95083b
unknown <juan.espinosa-t@labinfo.is.escuelaing.edu.co>
2022-02-23 16:54:14 -0500

    heroku deploy

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -9,47 +9,50 @@ public class HttpServer {
             System.err.println("Could not listen on port: 35000.");
             System.exit(1);
         }
-        Socket clientSocket = null;
-        try {
-            System.out.println("Listo para recibir ...");
-            clientSocket = serverSocket.accept();
-        } catch (IOException e) {
-            System.err.println("Accept failed.");
-            System.exit(1);
-        }
+        boolean running = true;
+        while(running) {
+            Socket clientSocket = null;
+            try {
+                System.out.println("Listo para recibir ...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
 
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(
-                        clientSocket.getInputStream()));
-        String inputLine, outputLine;
-        String file = "";
-        boolean firstLine = true;
-        while ((inputLine = in.readLine()) != null) {
-            if(firstLine){
-                file = inputLine.split(" ")[1];
-                System.out.println("File: " + file);
-                firstLine = false;
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(
+                            clientSocket.getInputStream()));
+            String inputLine, outputLine;
+            String file = "";
+            boolean firstLine = true;
+            while ((inputLine = in.readLine()) != null) {
+                if (firstLine) {
+                    file = inputLine.split(" ")[1];
+                    System.out.println("File: " + file);
+                    firstLine = false;
+                }
+                System.out.println("Received: " + inputLine);
+                if (!in.ready()) {
+                    break;
+                }
             }
-            System.out.println("Received: " + inputLine);
-            if (!in.ready()) {
-                break;
-            }
+            outputLine = "<!DOCTYPE html>"
+                    + "<html>"
+                    + "<head>"
+                    + "<meta charset=\"UTF-8\">"
+                    + "<title>Title of the document</title>\n"
+                    + "</head>"
+                    + "<body>"
+                    + "My Web Site"
+                    + "</body>"
+                    + "</html>" + inputLine;
+            out.println(outputLine);
+            out.close();
+            in.close();
+            clientSocket.close();
         }
-        outputLine = "<!DOCTYPE html>"
-                + "<html>"
-                + "<head>"
-                + "<meta charset=\"UTF-8\">"
-                + "<title>Title of the document</title>\n"
-                + "</head>"
-                + "<body>"
-                + "My Web Site"
-                + "</body>"
-                + "</html>" + inputLine;
-        out.println(outputLine);
-        out.close();
-        in.close();
-        clientSocket.close();
         serverSocket.close();
     }
     static int getPort() {
