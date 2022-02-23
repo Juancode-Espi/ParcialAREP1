d185cca72deaa952167416a613eef15a65d45182
unknown <juan.espinosa-t@labinfo.is.escuelaing.edu.co>
2022-02-23 16:43:02 -0500

    pom

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -4,7 +4,7 @@ public class HttpServer {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = null;
         try {
-            serverSocket = new ServerSocket(35000);
+            serverSocket = new ServerSocket(getPort());
         } catch (IOException e) {
             System.err.println("Could not listen on port: 35000.");
             System.exit(1);
@@ -51,5 +51,11 @@ public class HttpServer {
         in.close();
         clientSocket.close();
         serverSocket.close();
+    }
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set
     }
 }
